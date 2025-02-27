853e4db8d9477f92033c5f874dbf28eca24fcdf0
bruna.calves15 <bruna.calves15@L130635512W10-1>
2025-02-26 22:01:07 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleRevisaoAula/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleRevisaoAula
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Informe a primeira nota do aluno");
+            double nota1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Informe a segunda nota do aluno");
+            double nota2 = Convert.ToDouble(Console.ReadLine());
+
+            double media = (nota1 + nota2) / 2;
+
+            if (media >= 7)
+            {
+                Console.WriteLine("Aprovado");
+            }
+            else if (media < 5)
+            {
+                Console.WriteLine("Reprovado");
+            }
+            else
+            {
+                Console.WriteLine("Recuperação");
+            }
+
+        }
+    }
+}
---
+++ WFInicial/Form1.cs
@@ -0,0 +1,10 @@
+namespace WFInicial
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
