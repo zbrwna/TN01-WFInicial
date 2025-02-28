ccf8e9820e59931568723c6aad129aa9f91280cf
BRUNA COSTA ALVES <bruna.calves15@senacsp.edu.br>
2025-02-28 19:45:43 -0300

    feat: Exercicio 5 Corrigido no Console

---
+++ CEx5/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace CEx5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o 1º Numero: ");
+            int num1 = int.Parse(Console.ReadLine()!);
+            Console.Write("\nDigite o 2º Numero: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nDigite o 3º Numero: ");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+
+            int maior = Math.Max(num1, num2);
+            maior = Math.Max(maior, num3);
+
+            int menor = Math.Min(num1, num2);
+            maior = Math.Min(maior, num3);
+
+
+            Console.WriteLine($"\nO número {maior} é o maior valor!");
+            Console.WriteLine($"\nO número {menor} é o menor valor!");
+        }
+    }
+}
